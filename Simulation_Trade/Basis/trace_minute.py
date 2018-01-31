bed53d36c0999a7cb3cbeb58d7adaf2140a2b4f3
ildhj0116 <hongyimengli@163.com>
2018-01-31 17:11:31 +0800

    1.31

--- Simulation_Trade/Basis/trace_minute.py
+++ Simulation_Trade/Basis/trace_minute.py
@@ -17,19 +17,19 @@ plt.rcParams['axes.unicode_minus'] = False
 
 
 #设置多空合约以及手数、乘数
-cnt_list_str = "MA805.CZC,CS1805.DCE,ZC805.CZC,TA805.CZC,RU1805.SHF,J1805.DCE,RB1805.SHF,Y1805.DCE,BU1806.SHF,JM1805.DCE"
+cnt_list_str = "MA805.CZC,C1805.DCE,ZC805.CZC,A1805.DCE,L1805.DCE,J1805.DCE,RB1805.SHF,Y1805.DCE,BU1806.SHF,JM1805.DCE"
 cnt_list = cnt_list_str.split(',')
-long_list = "MA805.CZC,CS1805.DCE,ZC805.CZC,TA805.CZC,RU1805.SHF".split(",")
+long_list = "MA805.CZC,C1805.DCE,ZC805.CZC,A1805.DCE,L1805.DCE".split(",")
 short_list = "J1805.DCE,RB1805.SHF,Y1805.DCE,BU1806.SHF,JM1805.DCE".split(",")
-cnt_size = [13,18,6,13,3,-2,-9,-6,-13,-5]
+cnt_size = [13,21,6,10,8,-2,-9,-6,-13,-5]
 cnt_data = w.wss(cnt_list_str, "contractmultiplier")
 cnt_data = pd.DataFrame(cnt_data.Data,columns=cnt_data.Codes,index=cnt_data.Fields).T
 cnt_data["size"] = cnt_size
 
 
 #设置起止时间
-start_time = "2018-01-30 9:00:00"
-end_time = "2018-01-30 15:01:00"
+start_time = "2018-01-31 9:00:00"
+end_time = "2018-01-31 15:01:00"
 
 #提取分钟数据
 minute_data = w.wsi(cnt_list_str, "close", start_time, end_time, "")
@@ -60,7 +60,7 @@ x = minute_value.index
 x = x.to_pydatetime()
 x = np.vectorize(lambda s: s.strftime('%m-%d %H:%M'))(x)
 y = minute_value["ratio"].tolist()
-#设置横坐标显示
+#设置横坐标显示和标题
 xticks = range(0,len(x),10)
 xlabels = [x[index] for index in xticks]
 xticks.append(len(x))
@@ -68,15 +68,13 @@ xlabels.append(x[-1])
 ax1.set_xticks(xticks)
 ax1.set_xticklabels(xlabels,rotation=30)
 ax1.set_xlim(xticks[0],xticks[-1])
-ax1.set_title(u"多头总合约价值/空头总合约价值("+datetime.now().date().strftime("%Y-%m-%d")+")",fontsize=20)
+ax1.set_title(u"基差持仓多头总合约价值/空头总合约价值("+datetime.now().date().strftime("%Y-%m-%d")+")",fontsize=20)
+#画图，设置图片大小及分辨率、保存
 ax1.plot(x,y)
 fig.set_dpi(100)
 fig.set_figheight(10.8)
 fig.set_figwidth(19.2)
 plt.savefig("ratio.jpg")
-#from matplotlib.dates import DateFormatter  
-#yearsFmt = DateFormatter('%m-%d %H:%M')  
-#fig.autofmt_xdate()        #设置x轴时间外观  
-#ax1.xaxis.set_major_formatter(yearsFmt)      #设置时间显示格式
+
 
 
--- SpyderNet/OI_Construct.py
+++ SpyderNet/OI_Construct.py
@@ -14,8 +14,16 @@ w.start()
 def exchange_of_cmt(cmt):
     return cmt[-3:]
 
+def oi_download_process(date,df):
+    tmp_df = df[df["date"]==date].copy()
+    tmp_df.set_index("member_name",inplace=True)
+    tmp_df.drop([u"前五名合计",u"前十名合计",u"前二十名合计"],inplace=True)
+    #tmp_df.drop("date",axis=1,inplace=True)
+    return tmp_df
+
 def OI_Construct(main_cnt_df):
     all_cmt_oi_list = []
+    date_list = main_cnt_df.index
     for cmt in main_cnt_df.columns:
         cnt_series = main_cnt_df[cmt]
         unique_cnt_list = cnt_series.unique()
@@ -31,23 +39,60 @@ def OI_Construct(main_cnt_df):
                                      "member_name,long_position,short_position,vol")
                 
                 tmp_oi_data = pd.DataFrame(tmp_oi_data.Data, index=tmp_oi_data.Fields).T
-                tmp_oi_data.set_index(["date","member_name"],inplace=True)
-            #else:
-            oi_df_list.append(tmp_oi_data)
-        single_cmt_oi_data = pd.concat(oi_df_list)
-        single_cmt_oi_data.columns = pd.MultiIndex.from_product([[cmt],single_cmt_oi_data.columns])
-        all_cmt_oi_list.append(single_cmt_oi_data)    
+                #tmp_oi_data.set_index(["date","member_name"],inplace=True)
+                tmp_date_list = tmp_oi_data["date"].unique().tolist()
+                tmp_date_list.reverse()
+                for d in tmp_date_list:
+                    tmp_df = oi_download_process(d,tmp_oi_data)                    
+                    oi_df_list.append(tmp_df)
+            else:
+                tmp_long_data = w.wset("futureoir","startdate="+tmp_start_date+";enddate="+tmp_end_date+\
+                                     "varity="+cmt+";wind_code="+cnt+";order_by=long;ranks=all;field=date,"+\
+                                     "member_name,long_position")
+                tmp_long_data = pd.DataFrame(tmp_long_data.Data, index=tmp_long_data.Fields).T
+                tmp_short_data = w.wset("futureoir","startdate="+tmp_start_date+";enddate="+tmp_end_date+\
+                                     "varity="+cmt+";wind_code="+cnt+";order_by=short;ranks=all;field=date,"+\
+                                     "member_name,short_position")   
+                tmp_short_data = pd.DataFrame(tmp_short_data.Data, index=tmp_short_data.Fields).T
+
+                tmp_vol_data = w.wset("futurevir","startdate="+tmp_start_date+";enddate="+tmp_end_date+\
+                                     "varity="+cmt+";wind_code="+cnt+";ranks=all;field=date,"+\
+                                     "member_name,vol")            
+                tmp_vol_data = pd.DataFrame(tmp_vol_data.Data, index=tmp_vol_data.Fields).T
+                tmp_date_list = tmp_long_data["date"].unique().tolist()
+                tmp_date_list.reverse()
+                for d in tmp_date_list:
+                    tmp_long_df = oi_download_process(d,tmp_long_data)
+                    tmp_short_df = oi_download_process(d,tmp_short_data)
+                    tmp_vol_df = oi_download_process(d,tmp_vol_data)
+                    tmp_df = pd.concat([tmp_long_df,tmp_short_df,tmp_vol_df],axis=1)
+                    tmp_nan_df = tmp_df[tmp_df.T.isnull().any()]
+                    tmp_notnan_df = tmp_df[~tmp_df.T.isnull().any()].copy()
+                    tmp_notnan_df.loc["others"] = tmp_nan_df.sum()
+                    oi_df_list.append(tmp_notnan_df)
+            print cnt+"处理完毕"
+        tmp_series = pd.Series(oi_df_list,index=date_list,name=cmt)
+        all_cmt_oi_list.append(tmp_series)
+    oi_report_df = pd.concat(all_cmt_oi_list,axis=1)
+    return oi_report_df
+                    
+            
+#        single_cmt_oi_data = pd.concat(oi_df_list)
+#        single_cmt_oi_data.columns = pd.MultiIndex.from_product([[cmt],single_cmt_oi_data.columns])
+#        all_cmt_oi_list.append(single_cmt_oi_data)    
             
     
 if __name__ == "__main__":
-    main_cnt_df = pd.read_csv("main_cnt.csv",index_col=0)
-    main_cnt_df["date"] = [datetime.strptime(x,"%Y-%m-%d") for x in main_cnt_df.index]
-    start_date = datetime.strptime("2015-12-31","%Y-%m-%d")
+    main_cnt_df = pd.read_csv("main_cnt_without_DCE.csv",index_col=0)
+    main_cnt_df["date"] = [datetime.strptime(x,"%Y/%m/%d") for x in main_cnt_df.index]
+    start_date = datetime.strptime("2016-12-31","%Y-%m-%d")
     end_date = datetime.strptime("2018-1-25","%Y-%m-%d")
-    main_cnt_target = main_cnt_df[(main_cnt_df["date"]>start_date) & (main_cnt_df["date"]<end_date)]
+    main_cnt_target = main_cnt_df[(main_cnt_df["date"]>start_date) & (main_cnt_df["date"]<end_date)].copy()
     main_cnt_target.drop("date",axis=1,inplace=True)
-    OI_Construct(main_cnt_target)
-
+    df = OI_Construct(main_cnt_target)
+    
+    df.to_pickle("OI.tmp")
+    #try_df = pd.read_pickle("OI.tmp")
     
     """
     main_cnt_series_list = []    
