738194e2867f99aab7dd3764bba3e032f238f220
ildhj0116 <hongyimengli@163.com>
2018-02-05 09:03:22 +0800

    2.3

--- Simulation_Trade/Basis/trace_minute.py
+++ Simulation_Trade/Basis/trace_minute.py
@@ -17,19 +17,22 @@ plt.rcParams['axes.unicode_minus'] = False
 
 
 #设置多空合约以及手数、乘数
-cnt_list_str = "MA805.CZC,C1805.DCE,ZC805.CZC,A1805.DCE,L1805.DCE,J1805.DCE,RB1805.SHF,Y1805.DCE,BU1806.SHF,JM1805.DCE"
+cnt_list_str = "MA805.CZC,C1805.DCE,ZC805.CZC,A1805.DCE,RU1805.SHF,J1805.DCE,RB1805.SHF,Y1805.DCE,BU1806.SHF,JM1805.DCE"
 cnt_list = cnt_list_str.split(',')
-long_list = "MA805.CZC,C1805.DCE,ZC805.CZC,A1805.DCE,L1805.DCE".split(",")
+long_list = "MA805.CZC,C1805.DCE,ZC805.CZC,A1805.DCE,RU1805.SHF".split(",")
 short_list = "J1805.DCE,RB1805.SHF,Y1805.DCE,BU1806.SHF,JM1805.DCE".split(",")
-cnt_size = [13,21,6,10,8,-2,-9,-6,-13,-5]
+cnt_size = [13,21,6,10,3,-2,-9,-6,-13,-5]
 cnt_data = w.wss(cnt_list_str, "contractmultiplier")
 cnt_data = pd.DataFrame(cnt_data.Data,columns=cnt_data.Codes,index=cnt_data.Fields).T
 cnt_data["size"] = cnt_size
 
 
 #设置起止时间
-start_time = "2018-01-31 9:00:00"
-end_time = "2018-01-31 15:01:00"
+
+start_time = "2018-02-01 9:00:00"
+end_time = "2018-02-02 15:00:00"
+
+
 
 #提取分钟数据
 minute_data = w.wsi(cnt_list_str, "close", start_time, end_time, "")
@@ -49,26 +52,36 @@ minute_value["ratio"] = minute_value["long_value"]/(-minute_value["short_value"]
 minute_value["diff"] = minute_value["long_value"]+minute_value["short_value"]
 
 
+#导入前数据
+ratio = pd.read_csv("ratio.csv",index_col=0,names=["time","ratio"])
+ratio_TimeStamp = [datetime.strptime(x,"%Y/%m/%d %H:%M") for x in ratio.index]
+ratio_TimeStamp = [np.datetime64(x) for x in ratio_TimeStamp]
+ratio.index = ratio_TimeStamp
+intersection = ratio.index.intersection(minute_value.index)
+new_ratio = minute_value["ratio"].drop(intersection)
+all_ratio = pd.concat([ratio["ratio"],new_ratio])
+all_ratio.to_csv("ratio.csv")
+
 
 ###############################################################################
 #画图
 fig = plt.figure(1)
 ax1=fig.add_subplot(1,1,1)
 #minute_value["ratio"].plot(ax=ax1,title=u"多头总合约价值/空头总合约价值("+datetime.now().date().strftime("%Y-%m-%d")+")")
-x = minute_value.index
+x = all_ratio.index
 #将DatetimeIndex转为字符串
 x = x.to_pydatetime()
 x = np.vectorize(lambda s: s.strftime('%m-%d %H:%M'))(x)
-y = minute_value["ratio"].tolist()
+y = all_ratio.tolist()
 #设置横坐标显示和标题
-xticks = range(0,len(x),10)
+xticks = range(0,len(x),50)
 xlabels = [x[index] for index in xticks]
 xticks.append(len(x))
 xlabels.append(x[-1])
 ax1.set_xticks(xticks)
 ax1.set_xticklabels(xlabels,rotation=30)
 ax1.set_xlim(xticks[0],xticks[-1])
-ax1.set_title(u"基差持仓多头总合约价值/空头总合约价值("+datetime.now().date().strftime("%Y-%m-%d")+")",fontsize=20)
+ax1.set_title(u"基差持仓多头总合约价值/空头总合约价值",fontsize=20)
 #画图，设置图片大小及分辨率、保存
 ax1.plot(x,y)
 fig.set_dpi(100)
